c22964769ec5c4a4c0ef8a4bcb068164d4e428c5
rdx <31172722+rd-xx@users.noreply.github.com>
2023-05-21 21:22:29 +0200

    fix: rework snippet ui

--- src/components/generic/Snippet.jsx
+++ src/components/generic/Snippet.jsx
@@ -1,9 +1,9 @@
 const Snippet = ({ leftContent = "$", children }) => {
   return (
-    <div className="flex w-full flex-row items-center justify-between rounded-md border border-gray-200 bg-gray-100 px-4 py-2">
+    <div className="flex w-full flex-row items-center justify-between rounded-md border border-gray-200 bg-gray-100">
       <div className="flex flex-row items-center gap-x-4">
-        <div className="flex h-8 w-8 flex-row items-center justify-center rounded-md bg-gray-200">
-          {leftContent}
+        <div className="flex flex-row items-center justify-center rounded-l-md bg-gray-200 px-3 py-2">
+          <code>{leftContent}</code>
         </div>
         <div className="flex flex-col">{children}</div>
       </div>
